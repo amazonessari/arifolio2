c4964f540d228df6650c904019777e4519b13cee
amazonessari <bakaichi.arian@gmail.com>
2022-01-27 12:43:18 +0900

    Removed inport from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-// const withPlugins = require("next-compose-plugins");
-// const optimizedImages = require("next-optimized-images");
-import {withPlugins} from 'next-compose-plugins';
-import {optimizedImages} from 'next-optimized-images';
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
 
 module.exports = withPlugins([[optimizedImages, {}]], {
