3d375472cee6ea98999418ea9733fd7821746414
amazonessari <bakaichi.arian@gmail.com>
2022-01-27 12:28:42 +0900

    Removed require

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
+// const withPlugins = require("next-compose-plugins");
+// const optimizedImages = require("next-optimized-images");
+import {withPlugins} from 'next-compose-plugins';
+import {optimizedImages} from 'next-optimized-images';
 
+next-compose-plugins
 module.exports = withPlugins([[optimizedImages, {}]], {
   webpack: function (config) {
     config.module.rules.push({
